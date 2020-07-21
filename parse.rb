39411e0f3e191a955ecccf8012b4d3c17ba76b8f
Matteo Grassotti <matteo.grassotti@gmail.com>
2020-07-21 15:47:25 +0200

    Add ruby version
    and a ruby version check warning

--- parse.rb
+++ parse.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+
+app_ruby_version = File.read('.ruby-version')
+
+unless Gem::Version.new(RUBY_VERSION) >= Gem::Version.new(app_ruby_version)
+  puts "Warning: the script was developed using ruby #{app_ruby_version}"
+end
+
 require_relative 'parser'
 
 Parser.run(ARGV)
