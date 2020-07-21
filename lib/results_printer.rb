9bc46d8b7ea7b6db2b33fb6702e79578ca195362
Matteo Grassotti <matteo.grassotti@gmail.com>
2020-07-21 14:59:13 +0200

    Improve the output formatting
    Add a framed title to results

--- lib/results_printer.rb
+++ lib/results_printer.rb
@@ -6,6 +6,8 @@ require_relative 'visits_list'
 # and a list of webpages with most unique page views.
 # To be initialized with a websites parsed list (hash).
 class ResultsPrinter
+  PADDING = 23
+  TITLE_SPACING = 2
   attr_reader :list
 
   def initialize(visits_list)
@@ -20,12 +22,34 @@ class ResultsPrinter
   end
 
   def print_results(results, unique: false)
+    self.unique = unique
+    puts framed_title
     results.each do |page, visits|
-      n = unique ? visits[:unique_visits] : visits[:visits]
-      print "#{page} #{n} "\
-            "#{'unique ' if unique}"\
-            "#{n == 1 ? 'visit' : 'visits'} "
+      printf "%<page>-20s %<visits>s\n", page: page, visits: unique ? visits[:unique_visits] : visits[:visits]
     end
     print "\n"
   end
+
+  def calculate_paddings
+    self.title = "#{'Unique ' if unique}Visits"
+    (PADDING - title.length - TITLE_SPACING * 2).tap do |title_padding|
+      self.remainder_p = title_padding % 2
+      self.title_side_p = title_padding / 2
+    end
+  end
+
+  def framed_title
+    calculate_paddings
+    <<~TITLE
+      #{'#' * PADDING}
+      #{'#' * title_side_p}\
+      #{' ' * TITLE_SPACING}#{title}#{' ' * TITLE_SPACING}\
+      #{'#' * title_side_p}#{'#' * remainder_p}
+      #{'#' * PADDING}
+    TITLE
+  end
+
+  private
+
+  attr_accessor :unique, :title, :title_side_p, :remainder_p
 end
--- spec/lib/results_printer_spec.rb
+++ spec/lib/results_printer_spec.rb
@@ -4,16 +4,14 @@ RSpec.describe ResultsPrinter, '#run' do
   it 'ranks the pages list by visits' do
     list = LogParser.new('./spec/dummy/example.log').read
     expect { ResultsPrinter.new(list).run }.to output(
-      '/help_page/1 2 visits /contact 1 visit /home 1 visit /about/2 1 visit ' + "\n"\
-      '/help_page/1 2 unique visits /contact 1 unique visit /home 1 unique visit /about/2 1 unique visit ' + "\n"
+      File.read('./spec/dummy/example.output')
     ).to_stdout
   end
 
   it 'ranks the pages list by unique visits' do
     list = LogParser.new('./spec/dummy/example_unique_visits.log').read
     expect { ResultsPrinter.new(list).run }.to output(
-      '/help_page/1 3 visits /contact 2 visits /home 1 visit /about/2 1 visit ' + "\n"\
-      '/help_page/1 2 unique visits /contact 1 unique visit /home 1 unique visit /about/2 1 unique visit ' + "\n"
+      File.read('./spec/dummy/example_unique_visits.output')
     ).to_stdout
   end
 end
